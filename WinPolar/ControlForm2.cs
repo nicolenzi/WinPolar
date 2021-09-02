56ac6604ad5bdbd76e69b9ea099a58a3845412c3
nicolenzi <nicolenzi1@hotmail.com>
2021-09-02 20:13:39 -0300

    Se añadio una ventada de form extra
    ventana de form "controlform2" para configurar la aplicacion de winpolar antes de iniciar

---
+++ WinPolar/ControlForm2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinPolar
+{
+	public partial class ControlForm2 : Form
+	{
+		public ControlForm2()
+		{
+			InitializeComponent();
+		}
+	}
+}
--- WinPolar/Program.cs
+++ WinPolar/Program.cs
@@ -16,7 +16,10 @@ namespace WinPolar
 		{
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new ControlForm2());
 			Application.Run(new MascotaPolar());
+
+			
 		}
 	}
 }
